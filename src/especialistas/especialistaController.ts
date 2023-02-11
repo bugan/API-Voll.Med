e075a5f25e47378d71926f2a33c57fe5b26884c8
camilapessoa <camilabentespessoa@gmail.com>
2023-02-11 17:49:56 -0300

    feat: patch

--- src/especialistas/especialistaController.ts
+++ src/especialistas/especialistaController.ts
@@ -17,7 +17,8 @@ export const especialistas = async (
 }
 //Post 
 //verificar se o crm já existe
-export const especialistaPost = async (req: Request, res: Response): Promise<void> => {
+//Se o especialista for criado apenas com os atributos opcionais, enviar mensagem avisando quais campos faltam
+export const criarEspecialista = async (req: Request, res: Response): Promise<void> => {
   const {
     nome, crm, imagem, especialidade, email, telefone, nota
   } = req.body;
@@ -68,11 +69,8 @@ export const especialistaPut =async (req:Request, res:Response) => {
 };
 
 //Delete por id especialista/:id
-export const apagarEspecialista = async (
-  req: Request,
-  res: Response
-): Promise<void> => {
-  const { id } = req.params;
+export const apagarEspecialista = async (req: Request, res: Response): Promise<void> => {
+  const { id } = req.params
   const especialistaDel = await AppDataSource.manager.findOneBy(Especialista, {
      id:id ,
     })
@@ -83,5 +81,24 @@ export const apagarEspecialista = async (
      
       res.status(404).send("Id não encontrado");
     }
-}
+  }
 
+  //patch
+  export const atualizaContato = async (req:Request, res: Response): Promise<void> => {
+    const {id} = req.params
+    const buscaEspecialista = await AppDataSource.manager.findOneBy(Especialista, {id: id})
+    console.log('Id encontrado');
+    // res.send('Id encontrado')
+
+    const telefone = req.body.telefone
+
+    if(buscaEspecialista !== null){
+      buscaEspecialista.telefone = telefone
+      await AppDataSource.createQueryBuilder().update(Especialista, buscaEspecialista).where(buscaEspecialista.telefone).set({telefone:telefone}).execute()
+      res.status(200).json(buscaEspecialista)
+      }else{
+      res.status(400).send({message:'Contato não atualizado'})
+   
+    }
+           
+  }
