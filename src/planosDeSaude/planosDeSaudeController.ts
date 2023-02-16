55f0039a4ddf95c9e99562a5826d1c1ce42e4854
Gabrielle Ribeiro <gabrielleribeiro2010@gmail.com>
2023-02-16 16:49:00 -0300

    Altera planos de saúde adicionando literals no enum

--- src/planosDeSaude/planosDeSaudeController.ts
+++ src/planosDeSaude/planosDeSaudeController.ts
@@ -1,14 +1,14 @@
 import { type Request, type Response } from 'express'
 
 const planosSaude = {
-  0: 'Nenhum',
-  1: 'Sulamerica',
-  2: 'Unimed',
-  3: 'Bradesco',
-  4: 'Amil',
-  5: 'Biosaude',
-  6: 'Biovida',
-  7: 'Outro'
+  nenhum: 'NENHUM',
+  sulamerica: 'SULAMERICA',
+  unimed: 'UNIMED',
+  bradesco: 'BRADESCO',
+  amil: 'AMIL',
+  biosaude: 'BIOSAUDE',
+  biovida: 'BIOVIDA',
+  outro: 'OUTRO'
 }
 
 export const planosDeSaudeGet = async (req: Request, res: Response): Promise<void> => {
