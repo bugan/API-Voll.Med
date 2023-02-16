c27e134c963b5ba695544f7d7b08160b72fcb8c8
camilapessoa <camilabentespessoa@gmail.com>
2023-02-16 14:06:52 -0300

    update

--- src/data-source.ts
+++ src/data-source.ts
@@ -4,6 +4,7 @@ import 'reflect-metadata'
 import { Paciente } from './pacientes/pacienteEntity.js'
 import { Endereco } from './enderecos/enderecoEntity.js'
 import { Especialista } from './especialistas/EspecialistaEntidade.js';
+import { Avaliacoes } from './avaliacoes/avaliacoesEntity.js';
 import * as dotenv from 'dotenv'
 dotenv.config({ path: '.env' })
 export const AppDataSource = new DataSource({
@@ -15,7 +16,7 @@ export const AppDataSource = new DataSource({
   database: process.env.DB_DATABASE,
   synchronize: true,
   logging: false,
-  entities: [Paciente, Endereco, Especialista],
+  entities: [Paciente, Endereco, Especialista, Avaliacoes],
   migrations: [],
   subscribers: [],
 })
